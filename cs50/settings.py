f1e984056de86a9b829f0cad531932d31180a9f9
HosamAlmoghraby <32025281+HosamAlmoghraby@users.noreply.github.com>
2018-08-12 13:52:22 +0200

    final commit hello-django

--- cs50/settings.py
+++ cs50/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hello',
-    'render',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- cs50/urls.py
+++ cs50/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('hello.urls')),
-    path('index/', include('render.urls')),
     path('admin/', admin.site.urls),
 ]
